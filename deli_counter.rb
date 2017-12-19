907b18752e86a9a72e2eabf5fd15bb391100b9f8
github username <github email address>
2017-12-19 18:54:29 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,9 +17,7 @@ end
 
 def take_a_number (array, name)
   array.push(name)
-
-puts "Welcome, #{name}. You are number #{array.count} in line."
-
+  puts "Welcome, #{name}. You are number #{array.count} in line."
 end
 
 
